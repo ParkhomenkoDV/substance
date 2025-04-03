ee75749da2a59d511d1e2f8c4c7df036749135f1
ParkhomenkoDV <parkho.m.enko@mail.ru>
2025-04-03 16:37:13 +0300

    remove minutes in setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt', 'rt') as file:
 
 setup(
     name='substance',
-    version=time.strftime('%Y.%m.%d.%H.%M', time.localtime()),
+    version=time.strftime('%Y.%m.%d.%H', time.localtime()),
     description='lib',
     long_description=long_description,
     long_description_content_type='text/markdown',  # если long_description = .md
